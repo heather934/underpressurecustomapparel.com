53392bc25cb1b6c1c90e9ef9eee083a663631d32
heather934 <heather@underpressurecustomapparel.com>
2026-07-30 21:13:36 -0400

    update main.
    commit

--- underpressure-site/src/main.jsx
+++ underpressure-site/src/main.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { useEffect } from 'react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+export default function MainStore() {
+  useEffect(() => {
+    window.location.replace('/store.html')
+  }, [])
+  return null
+}
